250ecdc113da5da3b8661e8274378137e48c8b0f
Aashritha <s.aashritha14@gmail.com>
2025-09-11 14:22:29 +0530

    LCD

---
+++ VEGA_ARIES/G_LCD.c
@@ -0,0 +1,25 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+TwoWire Wire(1);
+LiquidCrystal_I2C lcd(0x27 , 16, 2);
+void setup()
+{
+lcd.init(); //initialize the lcd
+lcd.backlight();
+}
+void loop()
+{
+lcd.clear();    //clear display
+lcd.setCursor(0, 0);   //move cursor to (0,0)
+lcd.print("Greetings");  //print msg at (0,0)
+lcd.setCursor(2, 1);  //move cursor to (2,1)
+lcd.print("From ");   //print msg at (2,1)
+ delay(2000);
+
+ lcd.clear();       //clear display
+lcd.setCursor(3, 0);   //move cursor to (3,0)
+lcd.print("NIELIT");  //print msg at (3,0)
+lcd.setCursor(0, 1);  //move cursor to (0,1)
+lcd.print("Aurangabad ");   //print msg at (0,1)
+ delay(2000);
+}
